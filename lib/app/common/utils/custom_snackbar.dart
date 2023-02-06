17f623a782d16430e2eff76e7d9521ef2300d428
Urahaman, Mohamad Mujeeb [GCB-OT NE] <mu58995@citi.com>
2023-02-06 16:30:29 -0600

    implemented another_flushbar snackbar for callback messages display

--- lib/app/common/utils/custom_snackbar.dart
+++ lib/app/common/utils/custom_snackbar.dart
@@ -1,57 +1,58 @@
 import 'package:acscallingchatflutter/app/common/utils/constants.dart';
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 class CustomSnackBar {
+  static Flushbar? snackBar;
+
   void showToast(BuildContext context, String strMsg, bool status) async {
-    final snackBar = SnackBar(
-      behavior: SnackBarBehavior.floating,
-      margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height - 350),
-      duration: const Duration(seconds: 30),
-      dismissDirection: DismissDirection.startToEnd,
-      content: Container(
-        // margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        margin: EdgeInsets.fromLTRB(0, 0, 0, 75),
-        width: double.infinity,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: status ? AppColor.green_toast : AppColor.red_toast),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Container(
-                width: MediaQuery.of(context).size.width - 120,
-                child: Text(
-                  strMsg.toString(),
-                  style: TextStyle(fontSize: 13),
-                  softWrap: true,
-                  maxLines: 5,
-                  overflow: TextOverflow.fade,
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).removeCurrentSnackBar();
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                child: Icon(
-                  Icons.close,
-                  size: 20,
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
+    if (snackBar != null) {
+      return;
+    }
+    snackBar = Flushbar<bool>(
+      message: strMsg,
+      backgroundColor: status ? AppColor.green_toast : AppColor.red_toast,
+      margin: EdgeInsets.fromLTRB(16, 70, 16, 0),
+      borderRadius: BorderRadius.circular(10),
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.FLOATING,
+      reverseAnimationCurve: Curves.decelerate,
+      forwardAnimationCurve: Curves.decelerate,
+      isDismissible: false,
+      onStatusChanged: (status) => callBackStatusHandler(status),
+      mainButton: MaterialButton(
+        onPressed: () {
+          snackBar!.dismiss(true); // result = true
+        },
+        child: const Icon(
+          Icons.close,
+          size: 25,
+          color: Colors.white,
         ),
       ),
-      backgroundColor: Colors.transparent,
-      elevation: 1000,
-    );
+    ) // <bool> is the type of the result passed to dismiss() and collected by show().then((result){})
+      ..show(context).then((result) {});
+  }
 
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  void callBackStatusHandler(FlushbarStatus? status) {
+    switch (status!) {
+      case FlushbarStatus.SHOWING:
+        {
+          break;
+        }
+      case FlushbarStatus.IS_APPEARING:
+        {
+          break;
+        }
+      case FlushbarStatus.IS_HIDING:
+        {
+          break;
+        }
+      case FlushbarStatus.DISMISSED:
+        {
+          snackBar = null;
+          break;
+        }
+    }
   }
 }
