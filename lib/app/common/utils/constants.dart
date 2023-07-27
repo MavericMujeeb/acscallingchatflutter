93f85423bf7b512e61942821ddfa2e56e1e2e0ba
Urahaman, Mohamad Mujeeb [GCB-OT NE] <mu58995@citi.com>
2023-07-27 13:44:12 -0500

    changed client's name and profile pictures

--- lib/app/common/utils/constants.dart
+++ lib/app/common/utils/constants.dart
@@ -88,9 +88,9 @@ class Constants {
 
   //User Info data set for hardcoded, it should not break code in flutter
   //when user logged-in from iOS it'll be dynamic content
-  static String userName = 'Janet Johnson';
-  static String userEmail = 'janetjohnsonfamily83@gmail.com';
-  static String userId = 'a2194b29-07bb-48bb-8607-6151334cf904';
+  static String userName = 'Veronica Stephens';
+  static String userEmail = 'veronicastephens838@gmail.com';
+  static String userId = 'fc123292-abe0-4629-982e-1cfe16759cbb';
 
   static String bankerUserName = 'Richard Jones';
 
