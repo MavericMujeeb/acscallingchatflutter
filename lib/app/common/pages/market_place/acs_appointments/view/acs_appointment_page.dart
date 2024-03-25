632c67d467b12661331b721b21de74a46fe32cb1
Urahaman, Mohamad Mujeeb [GCB-OT NE] <mu58995@citi.com>
2024-03-25 13:11:46 -0500

    added data/pojo dart class from json object for access token

--- lib/app/common/pages/market_place/acs_appointments/view/acs_appointment_page.dart
+++ lib/app/common/pages/market_place/acs_appointments/view/acs_appointment_page.dart
@@ -1,15 +1,10 @@
-import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
-import 'package:flutter/material.dart';
 import 'package:acscallingchatflutter/app/common/pages/market_place/acs_appointments/view/phone/acs_appointment_phone_page.dart';
 import 'package:acscallingchatflutter/app/common/pages/market_place/acs_chat_calling/controller/acs_chat_calling_controller.dart';
-import 'package:acscallingchatflutter/app/common/pages/market_place/acs_chat_calling/view/phone/acs_chat_calling_phone_page.dart';
-import 'package:acscallingchatflutter/app/common/pages/market_place/acs_contact/view/phone/acs_contact_phone_page.dart';
 import 'package:acscallingchatflutter/data/repositories/acs_chat_calling_repositories.dart';
-// import 'package:marketplace/data/repositories/acs_chat_calling_repository.dart';
-import 'package:acscallingchatflutter/data/repositories/data_product_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
-
-class ACSAppointmentPage extends View{
+class ACSAppointmentPage extends View {
   const ACSAppointmentPage({Key? key}) : super(key: key);
 
   @override
@@ -18,9 +13,10 @@ class ACSAppointmentPage extends View{
   }
 }
 
-class ACSAppointmentPageState extends ResponsiveViewState<ACSAppointmentPage, ACSChatCallingController>{
-
-  ACSAppointmentPageState() : super(ACSChatCallingController(ACSChatCallingDataRepository()));
+class ACSAppointmentPageState
+    extends ResponsiveViewState<ACSAppointmentPage, ACSChatCallingController> {
+  ACSAppointmentPageState()
+      : super(ACSChatCallingController(ACSChatCallingDataRepository()));
 
   @override
   Widget get desktopView => const ACSAppointmentPhonePage();
@@ -33,5 +29,4 @@ class ACSAppointmentPageState extends ResponsiveViewState<ACSAppointmentPage, AC
 
   @override
   Widget get watchView => const ACSAppointmentPhonePage();
-
 }
